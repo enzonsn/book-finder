652bc35fbe84fcc642c21ed995f5ff25da598a48
enzonsn <enzonsperduti@gmail.com>
2021-07-30 17:11:54 -0500

    app.js compatable with apollo, loginForm and signupForm connected to mutations, api commmented out

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,22 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import {ApolloProvider} from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: { authorization: token ? `Bearer ${token}` : ''}
+    });
+  },
+  uri: 'graphql'
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
       <>
         <Navbar />
         <Switch>
@@ -15,7 +28,7 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+    </ApolloProvider>
   );
 }
 
