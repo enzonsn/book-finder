ebb6d98d6a0eea80258cc8d02d6cbc8369985672
enzonsn <enzonsperduti@gmail.com>
2021-07-30 16:49:24 -0500

    finished mutations and queries files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,19 @@
 import gql from 'graphql-tag';
 
-export const QUERY_ME = gql` `;
-
-export const QUERY_ME_DETAIL = gql` `;
+export const QUERY_ME = gql`
+    query {
+        me {
+            _id
+            username
+            bookCount
+            savedBooks {
+                bookId
+                image
+                link
+                title
+                authors
+                description
+            }
+        }
+    }
+`;
